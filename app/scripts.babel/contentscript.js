4c9f54ebd2d430b106c9fdeba859d5540350365e
Francisco Madrid <fjmadrid@gmail.com>
2017-04-08 15:59:21 -0300

    add news sites

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,31 +1,57 @@
 'use strict';
 
-var injectionEnabled = true;
+var newsSites = [
+  '9gag.com', // TODO: Delet this
+  'emol.com',
+  'elmostrador.cl',
+  'publimetro.cl',
+  'eldinamo.cl',
+  't13.cl',
+  'ciperchile.cl',
+  'lasegunda.com',
+];
+
+jQuery.each(newsSites, function (i) {
+  newsSites[i] = sprintf('[href*="%s"]', newsSites[i]);
+});
+
+var anchorSelector = sprintf('div[id*=hyperfeed_story_id] div[role=article] a%s', newsSites.join(','));
+
+var injectionTopNewsEnabled = true;
+var injectionMorePagerEnabled = true;
 
 var updateStories = function (element) {
-  var s = $(element).find('div[id*=hyperfeed_story_id] div[role=article] a[href*="9gag.com"],[href*="gph.is"]')
+  var s = $(element).find(anchorSelector)
     .closest('div[role=article]')
     .parent()
     .parent();
 
-  injectionEnabled = false;
+  injectionTopNewsEnabled = false;
+  injectionMorePagerEnabled = false;
+
   s.each(function (i, e) {
     if ($(e).parent().find('div[class="totb-container"]').length > 0) {
       return;
     }
     $(e).after('<div class="totb-container" style="margin: 10px 2px; padding: 10px 15px; background-color: black; color: white;">Esta es una prueba</div>');
   });
-  injectionEnabled = true;
+
+  injectionTopNewsEnabled = true;
+  injectionMorePagerEnabled = true;
 };
 
 $('body').on('DOMSubtreeModified', 'div[id*=topnews_main_stream]', function () {
-  if (injectionEnabled) {
+  if (injectionTopNewsEnabled) {
+    injectionTopNewsEnabled = false;
     updateStories(this);
+    injectionTopNewsEnabled = true;
   }
 });
 
 $('body').on('DOMSubtreeModified', 'div[id*=more_pager_pagelet]', function () {
-  if (injectionEnabled) {
+  if (injectionMorePagerEnabled) {
+    injectionMorePagerEnabled = false;
     updateStories(this);
+    injectionMorePagerEnabled = true;
   } 
 });
